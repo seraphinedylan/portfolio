8341c7ff8fbe1993cdc84ef9e7e71beb473869cf
seraphine <dylan.seraphine@elsan.care>
2024-06-09 18:01:01 +0200

    feat: add scroll down from home section

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,13 +1,18 @@
 import clsx from "clsx";
 import PropTypes from "prop-types";
 
-function Section({ className, children }) {
-  return <div className={clsx("min-h-dvh w-full", className)}>{children}</div>;
+function Section({ className, children, id }) {
+  return (
+    <div id={id} className={clsx("min-h-dvh w-full", className)}>
+      {children}
+    </div>
+  );
 }
 
 Section.propTypes = {
   children: PropTypes.node.isRequired,
   className: PropTypes.string,
+  id: PropTypes.string.isRequired,
 };
 
 export default Section;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,34 +3,49 @@ import {
   faPeopleGroup,
   faRocket,
 } from "@fortawesome/free-solid-svg-icons";
+import Lottie from "lottie-react";
 
+import MouseAnimation from "~/assets/animations/mouse.json";
 import Chip from "~/components/Chip";
 import Hero from "~/components/Hero";
 
 function Home() {
+  const scrollDown = () => {
+    const nextSection = document.getElementById("workSection");
+    nextSection.scrollIntoView({ behavior: "smooth" });
+  };
   return (
-    <div className="flex flex-col items-center justify-center gap-y-8 py-0 md:flex-row md:py-14">
-      <div className="to-background absolute inset-x-0 bottom-0 hidden h-1/4 bg-gradient-to-b from-transparent md:inline-block" />
-      <Hero
-        title="Hello, my name is Dylan Séraphine"
-        tagline="I am a Full Stack Developer who is currently based in Bordeaux, France."
-      >
-        <div className="hidden items-center space-x-3 md:flex">
-          <Chip startIcon={faCode} label="Lorem Ipsum" size="lg" />
-          <Chip startIcon={faPeopleGroup} label="Lorem Ipsum" size="lg" />
-          <Chip startIcon={faRocket} label="Lorem Ipsum" size="lg" />
+    <>
+      <div className="flex flex-col items-center justify-center md:flex-row">
+        <div className="absolute inset-x-0 bottom-0 hidden h-1/4 bg-gradient-to-b from-transparent to-background md:inline-block" />
+        <div
+          className="absolute bottom-4 hidden cursor-pointer justify-center md:flex"
+          onClick={scrollDown}
+        >
+          <Lottie animationData={MouseAnimation} className="h-auto w-14" />
         </div>
-      </Hero>
-      <div className="hidden px-4 md:inline-block md:px-0">
-        <img
-          alt="me"
-          width="480"
-          height="620"
-          src="src/images/Subject.png"
-          className=" object-cover object-top"
-        />
+        <Hero
+          title="Hello, my name is Dylan Séraphine"
+          tagline="I am a Full Stack Developer who is currently based in Bordeaux, France."
+        >
+          <div className="hidden items-center space-x-3 md:flex">
+            <Chip startIcon={faCode} label="Lorem Ipsum" size="lg" />
+            <Chip startIcon={faPeopleGroup} label="Lorem Ipsum" size="lg" />
+            <Chip startIcon={faRocket} label="Lorem Ipsum" size="lg" />
+          </div>
+        </Hero>
+
+        <div className="hidden px-4 md:inline-block md:px-0">
+          <img
+            alt="me"
+            width="480"
+            height="620"
+            src="src/images/Subject.png"
+            className=" object-cover object-top"
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
